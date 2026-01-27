94c40b4d3f65d25426c202e408ee0671e97c904b
otavio <otaviotemoteo@gmail.com>
2026-01-26 22:58:31 -0300

    chore: add initial exceptions

--- src/main/java/com/sales/management/exception/BusinessException.java
+++ src/main/java/com/sales/management/exception/BusinessException.java
@@ -1,5 +1,7 @@
 package com.sales.management.exception;
 
-public class BusinessException {
-
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/sales/management/exception/UnauthorizedException.java
+++ src/main/java/com/sales/management/exception/UnauthorizedException.java
@@ -1,5 +1,7 @@
 package com.sales.management.exception;
 
-public class UnauthorizedException {
-
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
 }
