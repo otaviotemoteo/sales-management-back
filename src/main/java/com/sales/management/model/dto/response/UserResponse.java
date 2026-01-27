6ba1493825b84cb9f2f9919096d2dfda5f7830c4
otavio <otaviotemoteo@gmail.com>
2026-01-26 23:04:09 -0300

    feat: add user initial configs

--- src/main/java/com/sales/management/model/dto/response/UserResponse.java
+++ src/main/java/com/sales/management/model/dto/response/UserResponse.java
@@ -1,5 +1,22 @@
 package com.sales.management.model.dto.response;
 
-public class UserResponse {
+import com.sales.management.model.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private UserRole role;
+    private Boolean active;
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/sales/management/model/enums/UserRole.java
+++ src/main/java/com/sales/management/model/enums/UserRole.java
@@ -1,16 +1,6 @@
 package com.sales.management.model.enums;
 
 public enum UserRole {
-    ADMIN("Administrador"),
-    SELLER("Vendedor");
-    
-    private final String displayName;
-    
-    UserRole(String displayName) {
-        this.displayName = displayName;
-    }
-    
-    public String getDisplayName() {
-        return displayName;
-    }
+    ADMIN,
+    SELLER
 }
